6334efdf9061a79a20317cd7c7fba8091bb02d40
TRAING Serey <t.sereypich@gmail.com>
2018-05-23 16:43:28 +0200

    first commit

---
+++ bomberman/bomberman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bomberman
+//
+//  Created by TRAING Serey on 23/05/2018.
+//  Copyright © 2018 AlkRox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
